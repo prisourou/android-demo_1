3e47dac6f541e47c4753d7b89570e7e5e1ea257b
Prisca <priscaahouandjinou06@gmail.com>
2021-11-25 10:06:19 +0100

    Calculatrice

---
+++ app/src/main/java/prisourou/e/demo_1/Calculatrice.java
@@ -0,0 +1,86 @@
+package prisourou.e.demo_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Calculatrice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculatrice);
+    }
+}
+public void clearOnClik(View view){
+
+}
+public void  bracketsOnClik(View view){
+
+}
+
+public void purcentOnClik(View view){
+
+}
+
+public void divisiOnClik(View view{
+
+}
+
+public void seOnClik(View view){
+
+}
+public void eOnClik(View view){
+
+}
+
+public void niOnClik(View view){
+
+}
+
+public void timeOnClik(View view){
+
+}
+
+public void fouOnClik(View view){
+
+}
+public void fivOnClik(View view){
+
+}
+
+public void sixOnClik(View view){
+
+}
+public void souOnClik(View view){
+
+}
+
+public void onOnClik(View view){
+
+}
+
+public void twClik(View view){
+
+}
+public void thOnClik(View view) {
+
+}
+
+public void plusOnClik(View view){
+
+}
+
+public  void zeOnClik(View view){
+
+}
+public  void comOnClik(View view){
+
+}
+
+public void eOnClik(View view){
+
+}
+
+
